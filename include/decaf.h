710cd1ffde6bdc71e07af2851b7896c764479fc8
Sean Lynch <phantom@lynches.ca>
2020-10-02 17:46:52 -0400

    Add code skeleton (#5)
    This sets up the basic interpreter and stack data structures.
    Still need code to implement the main loop and such.

---
+++ include/decaf.h
@@ -0,0 +1,64 @@
+
+#include <llvm/IR/InstVisitor.h>
+
+#include <z3++.h>
+
+#include <cassert>
+#include <vector>
+#include <unordered_map>
+
+namespace decaf {
+  class StackFrame {
+  public:
+    std::unordered_map<llvm::Value*, z3::expr> variables;
+    
+    llvm::Function* function;
+    llvm::BasicBlock* current_block;
+    llvm::BasicBlock* prev_block;
+    // Need a current instruction pointer (maybe block iterator?)
+  };
+
+  class Context {
+  public:
+    std::vector<StackFrame> stack;
+
+    Context fork() const {
+      assert("not implemented");
+      std::exit(EXIT_FAILURE);
+    }
+  };
+
+  enum class ExecutionResult {
+    Continue,
+    Stop,
+    Unimplemented
+  };
+
+  class Interpreter : public llvm::InstVisitor<Interpreter, ExecutionResult> {
+  private:
+    Context* ctx; // Non-owning context reference
+
+  public:
+    // Add some more parameters here
+    Interpreter();
+
+    ExecutionResult visitInstruction(llvm::Instruction& I) {
+      assert("Instruction not implemented");
+      return ExecutionResult::Unimplemented;
+    }
+
+    // Replace this with implementation in cpp file as we go
+    ExecutionResult visitAdd(llvm::BinaryOperator& op) { return ExecutionResult::Unimplemented; }
+    ExecutionResult visitSub(llvm::BinaryOperator& op) { return ExecutionResult::Unimplemented; }
+    ExecutionResult visitMul(llvm::BinaryOperator& op) { return ExecutionResult::Unimplemented; }
+    ExecutionResult visitUDiv(llvm::BinaryOperator& op) { return ExecutionResult::Unimplemented; }
+    ExecutionResult visitSDiv(llvm::BinaryOperator& op) { return ExecutionResult::Unimplemented; }
+    ExecutionResult visitURem(llvm::BinaryOperator& op) { return ExecutionResult::Unimplemented; }
+    ExecutionResult visitSRem(llvm::BinaryOperator& op) { return ExecutionResult::Unimplemented; }
+
+    ExecutionResult visitPHINode(llvm::PHINode& node) { return ExecutionResult::Unimplemented; }
+    ExecutionResult visitBranchInst(llvm::BranchInst& inst) { return ExecutionResult::Unimplemented; }
+    ExecutionResult visitReturnInst(llvm::ReturnInst& inst) { return ExecutionResult::Unimplemented; }
+    ExecutionResult visitCallInst(llvm::CallInst& inst) { return ExecutionResult::Unimplemented; }
+  };
+}
---
+++ src/decaf.cpp
@@ -0,0 +1,2 @@
+
+#include "decaf.h"
