56a13dfd94fb139547d0b17f829b9597b89f3c02
Phantomical <phantom@lynches.ca>
2020-10-10 01:18:17 -0400

    Implement main loop to execute a function symbolically
    This sets up the external API that our executable will use.
    Currently there's no way to report the failures beyond printing
    them to stdout but for a proof of concept that's probably all that's
    needed.

--- src/decaf.cpp
+++ src/decaf.cpp
@@ -5,8 +5,12 @@
 #include <llvm/Support/raw_ostream.h>
 
 #include <iostream>
+#include <stdexcept>
 
 namespace decaf {
+  /************************************************
+   * Executor                                     *
+   ************************************************/
   void Executor::add_context(Context&& ctx) {
     contexts.push_back(ctx);
   }
@@ -31,6 +35,42 @@ namespace decaf {
     return !contexts.empty();
   }
 
+  /************************************************
+   * StackFrame                                   *
+   ************************************************/
+  StackFrame::StackFrame(llvm::Function* function) :
+    function(function),
+    current_block(&function->getEntryBlock()),
+    prev_block(nullptr),
+    current(current_block->begin())
+  {}
+
+  /************************************************
+   * Context                                      *
+   ************************************************/
+  Context::Context(z3::context& z3, llvm::Function* function) :
+    solver(z3)
+  {
+    stack.emplace_back(function);
+    StackFrame& frame = stack_top();
+
+    int argnum = 0;
+    for (auto& arg : function->args()) {
+      z3::sort sort = sort_for_type(z3, arg.getType());
+      z3::symbol symbol = z3.int_symbol(argnum);
+
+      frame.variables.insert({ &arg, z3.constant(symbol, sort) });
+      argnum += 1;
+    }
+  }
+
+
+  /************************************************
+   * Interpreter                                  *
+   ************************************************/
+  Interpreter::Interpreter(Context* ctx, Executor* queue, z3::context* z3) :
+    ctx(ctx), queue(queue), z3(z3)
+  {}
 
   void Interpreter::execute() {
     ExecutionResult exec;
@@ -51,5 +91,42 @@ namespace decaf {
 
       exec = visit(inst);
     } while (exec == ExecutionResult::Continue);
+  }
+
+  /************************************************
+   * Free Functions                               *
+   ************************************************/
+  z3::sort sort_for_type(z3::context& ctx, llvm::Type* type) {
+    if (type->isIntegerTy()) {
+      return ctx.bv_sort(type->getIntegerBitWidth());
+    }
+
+    std::string message;
+    llvm::raw_string_ostream os{message};
+    os << "Unsupported LLVM type: ";
+    type->print(os);
+
+    DECAF_ABORT(message);
+  }
+
+  void execute_symbolic(llvm::Function* function) {
+    z3::config cfg;
+
+    // We want Z3 to generate models
+    cfg.set("model", true);
+    // Automatically select and configure the solver
+    cfg.set("auto_config", true);
+
+    z3::context z3{cfg};
+    Executor exec;
+
+    exec.add_context(Context(z3, function));
+
+    while (exec.has_next()) {
+      Context ctx = exec.next_context();
+      Interpreter interp{&ctx, &exec, &z3};
+
+      interp.execute();
+    }
   }
 }
