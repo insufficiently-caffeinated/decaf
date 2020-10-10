3c3276fdab92875faccdbce948bfb8be072e0147
Phantomical <phantom@lynches.ca>
2020-10-10 00:28:21 -0400

    Implement methods on Executor

--- include/decaf.h
+++ include/decaf.h
@@ -39,11 +39,17 @@ namespace decaf {
   public:
     // The current context has forked and the fork needs to be added
     // to the queue.
-    void add_context(Context&& ctx) {}
+    void add_context(Context&& ctx);
 
     // The current context has encountered a failure that needs to be
     // recorded.
-    void add_failure(const z3::model& model) {}
+    void add_failure(const z3::model& model);
+
+    // Get the next context to be executed.
+    Context next_context();
+
+    // Are there any contexts left?
+    bool has_next() const;
   };
 
   enum class ExecutionResult {
--- src/decaf.cpp
+++ src/decaf.cpp
@@ -2,8 +2,36 @@
 #include "decaf.h"
 
 #include <llvm/IR/Instructions.h>
+#include <llvm/Support/raw_ostream.h>
+
+#include <iostream>
 
 namespace decaf {
+  void Executor::add_context(Context&& ctx) {
+    contexts.push_back(ctx);
+  }
+
+  void Executor::add_failure(const z3::model& model) {
+    // TODO: It might not be necessary for the prototype but we'll need
+    //       to figure what we want to do with test failures.
+    std::cout
+      << "Found failed model! Inputs: \n"
+      << model << std::endl;
+  }
+
+  Context Executor::next_context() {
+    DECAF_ASSERT(has_next());
+
+    Context ctx = std::move(contexts.back());
+    contexts.pop_back();
+    return ctx;
+  }
+
+  bool Executor::has_next() const {
+    return !contexts.empty();
+  }
+
+
   void Interpreter::execute() {
     ExecutionResult exec;
 
